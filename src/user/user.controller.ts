d9a2a01b391f166e858bf8dd85e9ce0cd45174bc
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-06-02 13:58:57 +0200

    feat(user): implement controller with all endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAll(): Promise<User[]> {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<User> {
+    return this.userService.getById(id);
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto.login, createUserDto.password);
+  }
+
+  @Put(':id')
+  async updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ): Promise<User> {
+    return this.userService.updatePassword(
+      id,
+      updatePasswordDto.oldPassword,
+      updatePasswordDto.newPassword,
+    );
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.userService.delete(id);
+  }
+}
