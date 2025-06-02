79b617719d23d09ee023e99517d7f07643139a3a
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-06-02 22:10:02 +0200

    feat(favorites): implement service with crud

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FavoritesController } from './favorites.controller';
 import { FavoritesService } from './favorites.service';
+import { TrackRepository } from '../track/track.repository';
+import { AlbumRepository } from '../album/album.repository';
+import { ArtistRepository } from '../artist/artist.repository';
 
 @Module({
   controllers: [FavoritesController],
-  providers: [FavoritesService]
+  providers: [
+    FavoritesService,
+    TrackRepository,
+    AlbumRepository,
+    ArtistRepository,
+  ],
 })
 export class FavoritesModule {}
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Favorites } from './entities/favorites.entity';
+import { TrackRepository } from '../track/track.repository';
+import { AlbumRepository } from '../album/album.repository';
+import { ArtistRepository } from '../artist/artist.repository';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  private favorites: Favorites = {
+    artists: [],
+    albums: [],
+    tracks: [],
+  };
+
+  constructor(
+    private readonly trackRepository: TrackRepository,
+    private readonly albumRepository: AlbumRepository,
+    private readonly artistRepository: ArtistRepository,
+  ) {}
+
+  getAll(): Favorites {
+    return this.favorites;
+  }
+
+  addTrack(id: string): void {
+    this.trackRepository.findById(id);
+    if (!this.favorites.tracks.includes(id)) {
+      this.favorites.tracks.push(id);
+    }
+  }
+
+  removeTrack(id: string): void {
+    const index = this.favorites.tracks.indexOf(id);
+    if (index === -1) throw new NotFoundException('Track is not in favorites');
+    this.favorites.tracks.splice(index, 1);
+  }
+
+  addAlbum(id: string): void {
+    this.albumRepository.findById(id);
+    if (!this.favorites.albums.includes(id)) {
+      this.favorites.albums.push(id);
+    }
+  }
+
+  removeAlbum(id: string): void {
+    const index = this.favorites.albums.indexOf(id);
+    if (index === -1) throw new NotFoundException('Album is not in favorites');
+    this.favorites.albums.splice(index, 1);
+  }
+
+  addArtist(id: string): void {
+    this.artistRepository.findById(id);
+    if (!this.favorites.artists.includes(id)) {
+      this.favorites.artists.push(id);
+    }
+  }
+
+  removeArtist(id: string): void {
+    const index = this.favorites.artists.indexOf(id);
+    if (index === -1) throw new NotFoundException('Artist is not in favorites');
+    this.favorites.artists.splice(index, 1);
+  }
+}
