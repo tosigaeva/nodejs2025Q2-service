d16a8404b36702dc47e85e09891913f0ec220842
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-06-02 17:17:04 +0200

    feat(artist): implement repository and service with crud

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { ArtistRepository } from './artist.repository';
+import { Artist } from './entities/artist.entity';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+  constructor(private readonly artistRepository: ArtistRepository) {}
+
+  getAll(): Artist[] {
+    return this.artistRepository.findAll();
+  }
+
+  getById(id: string): Artist {
+    return this.artistRepository.findById(id);
+  }
+
+  create(name: string, grammy: boolean): Artist {
+    if (!name?.trim()) {
+      throw new BadRequestException('Name is required');
+    }
+    return this.artistRepository.create(name, grammy);
+  }
+
+  update(id: string, data: Partial<Artist>): Artist {
+    return this.artistRepository.update(id, data);
+  }
+
+  delete(id: string): void {
+    this.artistRepository.delete(id);
+  }
+}
